dc0be4600677e956865df4d6d905c3809d64b93b
Kristian Ivanov <kriskopk@abv.bg>
2022-10-03 15:38:07 +0300

    created tabs Customers/Orders

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import TabNavigator from './TabNavigator';
 
+export type RootStackParamList ={
+  Main: undefined;
+  MyModal: {userId: string; name: string;}
+  Order: {order:any}
+}
+
 const RootStack = createNativeStackNavigator();
 
 const RootNavigator = () => {
